d54467b01b4ba1441e779de37446b38f143a717d
aeroderp <1325790@learn.vsb.bc.ca>
2025-11-16 23:40:21 +0000

    f

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -1,4 +1,10 @@
 from PIL import Image
-
-image_green = Image.open("CS11/5.1/kid-green.jpg").load()
-image_beach = Image.open("CS11/5.1/beach.jpg").load()
+image_clock = Image.open("clock.jpg").load()
+image_output = Image.open("kid-green.jpg")
+width = image_output.width
+height = image_output.height
+for i in range(width):
+    for j in range(height):
+        im_r = image_green[i, j][0]
+        im_g = image_green[i, j][1]
+        im_b = image_green[i, j][2]
