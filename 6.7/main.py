81241b3879f4673dd0e44e1458a145e4f88368ca
aeroderp <1325790@learn.vsb.bc.ca>
2025-12-02 21:14:17 +0000

    lol

--- 6.7/main.py
+++ 6.7/main.py
@@ -2,38 +2,38 @@ from PIL import Image
 import time
 t0 = time.time()
 def colour(r, g, b):
-    if 40 < r < 80 and 80 < g < 140 and b <= 150:
+    if r <= 100 and g <= 200 and b <= 250:
         return "blue"
     else:
         return "other"
 
 
-file = Image.open("6.7/ocean.png")
-jb_image = file.load()
-
-
-image_output = Image.open("6.7/ocean.png")
-
 blue_pixels = []
 
-width = file.width
-height = file.height
+image_list = ["ocean1.png", "ocean2.jpg", "ocean3.jpg" "ocean4.jpg" "ocean5.webp" "ocean6.png" "ocean7.png" "ocean8.png" "ocean9.png" "ocean10.png"]
 
-for x in range(width):
-    for y in range(height):
-        r = jb_image[x, y][0]
-        g = jb_image[x, y][1]
-        b = jb_image[x, y][2]
-        
-        if colour(r,g,b) == "blue":
-            blue_pixels.append(jb_image[x,y])
-t1 = time.time()
-time_taken = t1 - t0
-percent_blue = (len(blue_pixels)/(width*height))*100
-timing = "This program took {:.2f} seconds to run.".format(time_taken)
-print(timing)
-print("there are " + str(len(blue_pixels)) + " blue pixels in the image")
-print("there are " + str(width*height) + " total pixels in the image")
-percentage = "the percentage of blue pixels is {:.2f}%".format(percent_blue)
-print(percentage)
-image_output.save("ocean_output.png", "png")
+for image in image_list:
+    file = Image.open("6.7/" + image)
+    width = file.width
+    height = file.height
+    jb_image = file.load()
+
+
+    for x in range(width):
+        for y in range(height):
+            r = jb_image[x, y][0]
+            g = jb_image[x, y][1]
+            b = jb_image[x, y][2]
+            
+            if colour(r,g,b) == "blue":
+                blue_pixels.append(jb_image[x,y])
+    t1 = time.time()
+    time_taken = t1 - t0
+    percent_blue = (len(blue_pixels)/(width*height))*100
+    timing = "This program took {:.2f} seconds to run.".format(time_taken)
+    print(timing)
+    print("there are " + str(len(blue_pixels)) + " blue pixels in the image")
+    print("there are " + str(width*height) + " total pixels in the image")
+    percentage = "the percentage of blue pixels is {:.2f}%".format(percent_blue)
+    print(percentage)
+    image_output.save("ocean_output.png", "png")
