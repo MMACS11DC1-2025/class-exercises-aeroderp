8f687f79c00e7ca51149e4b850d790c63a479966
aeroderp <1325790@learn.vsb.bc.ca>
2025-12-15 06:45:02 +0000

    commiting

--- 6.7/main.py
+++ 6.7/main.py
@@ -1,6 +1,5 @@
 from PIL import Image
 import time
-t0 = time.time()
 def colour(r, g, b):
     if r <= 100 and g <= 200 and b <= 250:
         return "blue"
@@ -11,15 +10,18 @@ def colour(r, g, b):
 
 image_list = ["ocean1.jpg", "ocean2.jpg", "ocean3.jpg", "ocean4.jpg", "ocean5.webp", "ocean6.webp", "ocean7.jpg", "ocean8.webp", "ocean9.jpg", "ocean10.jpg"]
 
+results = []
+
 for i in range(len(image_list)):
+    t0 = time.time()
     image = image_list[i]
     file = Image.open("6.7/" + image)
-    blue_pixels = []
+    blue_pixels = 0
     width = file.width
     height = file.height
     jb_image = file.load()
 
-
+    
     for x in range(width):
         for y in range(height):
             r = jb_image[x, y][0]
@@ -27,14 +29,45 @@ for i in range(len(image_list)):
             b = jb_image[x, y][2]
             
             if colour(r,g,b) == "blue":
-                blue_pixels.append(jb_image[x,y])
+                blue_pixels += 1
     t1 = time.time()
+    
     time_taken = t1 - t0
-    percent_blue = (len(blue_pixels)/(width*height))*100
+    percent_blue = (blue_pixels/(width*height))*100
 
     timing = "ocean" + str(i+1) +  " took {:.2f} seconds to run.".format(time_taken)
     print(timing)
-    print("there are " + str(len(blue_pixels)) + " blue pixels in the image")
+    print("there are " + str(blue_pixels) + " blue pixels in the image")
     print("there are " + str(width*height) + " total pixels in the image")
     print("the percentage of blue pixels is {:.2f}%\n".format(percent_blue))
+    results.append((image, percent_blue))
+
+# ranking the results
+for i in range (len(results)):
+    biggest_index = i
+
+    for j in range (i+1, len(results)):
+        if results[j][1] > results[biggest_index][1]:
+            biggest_index = j
+    results[i], results[biggest_index] = results[biggest_index], results[i]
+
+# binary search for minimum percentage wanted
+def search_min_results(results, min_percent):
+    minimum = 0
+    maximum = len(results)-1
+
+    while minimum <= maximum:
+        mid = (minimum + maximum) // 2
+
+        if results[mid][1] = min_percent:
+            return mid
+        elif results[mid][1] < min_percent:
+            maximum = mid - 1
+        else:
+            minimum = mid + 1
+
+    
+    return -1
+print(search_min_results(results, 30))
+
     
--- practice/main.py
+++ practice/main.py
@@ -1,8 +1,12 @@
-list = [4,2,6,7,1,5,2,3]
-list2 = list[:]
-highest = []
-for i in range (int(list2)):
-    if list[i] <= 0:
-        ammount = list[i]
-        highest += ammount
-        
+scores = [4,2,6,7,1,5,2,3]
+for i in range (len(scores)):
+
+    smallest_score = scores[i]
+    smallest_index = i
+    for j in range (i+1, len(scores)):
+        if scores [j] < smallest_score:
+            smallest_score = scores[j]
+            smallest_index = j
+    scores[i], scores[smallest_index] = scores[smallest_index], scores[i],
+print(scores)
+
