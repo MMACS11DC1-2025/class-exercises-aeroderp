b1100db8d8e0e9c8ee3489e42aa80ffbdb400986
aeroderp <1325790@learn.vsb.bc.ca>
2025-12-16 21:15:36 +0000

    committt

--- 6.7/main.py
+++ 6.7/main.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import time
-def colour(r, g, b):
+def is_target_colour(r, g, b):
     if r <= 100 and g <= 200 and b <= 250:
         return "blue"
     else:
@@ -28,14 +28,14 @@ for i in range(len(image_list)):
             g = jb_image[x, y][1]
             b = jb_image[x, y][2]
             
-            if colour(r,g,b) == "blue":
+            if is_target_colour(r,g,b) == "blue":
                 blue_pixels += 1
     t1 = time.time()
     
     time_taken = t1 - t0
     percent_blue = (blue_pixels/(width*height))*100
 
-    timing = "ocean" + str(i+1) +  " took {:.2f} seconds to run.".format(time_taken)
+    timing = "ocean" + str(i+1) +  " took {:.3f} seconds to run.".format(time_taken)
     print(timing)
     print("there are " + str(blue_pixels) + " blue pixels in the image")
     print("there are " + str(width*height) + " total pixels in the image")
